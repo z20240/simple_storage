d5bbfe741536e02a29288086070a8df8667ffc50
les <z20240z@gmail.com>
2018-05-21 15:05:39 +0800

    add middleware cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Cors = require('./middleware');
 
 var cacheRouter = require('./routes/cache');
 
@@ -13,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(Cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
